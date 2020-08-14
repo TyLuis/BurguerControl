0ccb2f9b7917133c4506fdf8187e68b086e12e47
Luís Augusto Borges <tyluisaugusto@gmail.com>
2020-08-14 15:55:13 -0300

    Continuação do front-end
    Diversas alterações na activity pedido e adição de outras

---
+++ app/src/main/java/com/example/BurguerControl/detalhesPedido.java
@@ -0,0 +1,14 @@
+package com.example.BurguerControl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detalhesPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_pedido);
+    }
+}
