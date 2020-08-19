0a1829710f6da3dee95dbbd91ecb85c848a28f6c
Luís Augusto Borges <tyluisaugusto@gmail.com>
2020-08-19 18:19:47 -0300

    Continuação do front-end
    finalização do crud

---
+++ app/src/main/java/com/example/BurguerControl/outrosProdutos.java
@@ -0,0 +1,14 @@
+package com.example.BurguerControl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class outrosProdutos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outros_produtos);
+    }
+}
