0c8a127792b9a2155e5f662cf28be851870e770b
Luís Augusto Borges <tyluisaugusto@gmail.com>
2020-08-24 17:21:06 -0300

    Configuração FireBase
    Configuração do banco de dados FireBase no aplicativo

--- app/src/main/java/com/example/BurguerControl/addEditIngrediente.java
+++ app/src/main/java/com/example/BurguerControl/addEditIngrediente.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-public class MainActivity extends AppCompatActivity {
+public class addEditIngrediente extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_edit_ingrediente);
     }
 }
--- app/src/main/java/com/example/BurguerControl/criarConta.java
+++ app/src/main/java/com/example/BurguerControl/criarConta.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-public class MainActivity extends AppCompatActivity {
+public class criarConta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_criar_conta);
     }
 }
