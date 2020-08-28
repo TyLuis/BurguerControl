a0b59b0882ea1c1b8654bc1e42492cdd09dc9f67
Luís Augusto Borges <tyluisaugusto@gmail.com>
2020-08-28 14:22:02 -0300

    Começo do back-end

---
+++ app/src/main/java/com/example/BurguerControl/Usuario.java
@@ -0,0 +1,47 @@
+package com.example.BurguerControl;
+
+public class Usuario {
+    private Integer idUsuario;
+    private String nomeUsuario;
+    private String emailUsuario;
+    private String senhaUsuario;
+
+    public Usuario(Integer idUsuario, String nomeUsuario, String emailUsuario, String senhaUsuario) {
+        this.idUsuario = idUsuario;
+        this.nomeUsuario = nomeUsuario;
+        this.emailUsuario = emailUsuario;
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEmailUsuario() {
+        return emailUsuario;
+    }
+
+    public void setEmailUsuario(String emailUsuario) {
+        this.emailUsuario = emailUsuario;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+}
--- app/src/main/java/com/example/BurguerControl/criarConta.java
+++ app/src/main/java/com/example/BurguerControl/criarConta.java
@@ -1,14 +1,36 @@
 package com.example.BurguerControl;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class criarConta extends AppCompatActivity {
+    private FirebaseAuth autentica;
+    private Object FirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_conta);
+        autentica = FirebaseAuth.getInstance();
+
+        String email;
+        String senha;
+        autentica.createUserWithEmailAndPassword(email,senha)
+                .addOnCompleteListener(criarConta.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    }
+                });
     }
 }
